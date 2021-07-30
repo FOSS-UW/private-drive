1110b0bb9ae0bdc02419629bfbd15a43c19a917c
stephpavlou <stephpavlou@gmail.com>
2021-07-29 21:07:21 -0500

    new home

--- public/home.php
+++ public/home.php
@@ -7,15 +7,23 @@
             if (!isset($_SESSION['user'])) {
                 header('Location: index.php');
                 session_destroy();
-            } else {
-                echo "Welcome back, " . $_SESSION['user'];
             }
         ?>
     </head>
     <body>
+	<div id="upload-form">
+		<form enctype="multipart/form-data" action="file.upload.php"
+			method="POST">
+			<input type="hidden" name="MAX_FILE_SIZE" value="20000000"/>
+			Upload file: <input name="upload-file" type="file"/>
+			File destination: <input name="dest-path" type="text"/>
+			<input type="submit" value="Upload"/>
+		</form>
+	</div>
 	<div id="content">
 		<?php
-			require 'on.load.php';
+	    		require 'on.load.php';
+			echo '<i>Welcome back,</i> ' . $_SESSION['user'];			
     			on_load($_SESSION['user']); 	    
 		?>
 	</div>
